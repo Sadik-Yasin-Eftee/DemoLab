29da5f9393dbbf58e1cdef573658c169f7650ed0
Sadik-Yasin-Eftee <sadikyasin492@gmail.com>
2023-02-09 12:53:09 +0600

    env file added

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 
+require("dotenv").config({path:"./.env"});
+
 app.get('/',(req,res) =>{
     res.send('Welcome to azure');
 })
 
 //port 3000
-app.listen(3000,()=>{
+app.listen(process.env.port,()=>{
     console.log('Server is running');
 })
